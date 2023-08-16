ddfa2abdc5fd9ab0c537be3ee968e6dcf7ffdd2b
Quokka-Wakka <141393982+Quokka-Wakka@users.noreply.github.com>
2023-08-16 23:52:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const button = document.querySelector(".big-button");
   const marquee = document.querySelector(".marquee-text");
   button.addEventListener("click", function () {
-    fetch("https://maker.ifttt.com/trigger/kutty_kutty/with/key/dcAC4G14mWOjZpmkrhrB63");
+    fetch("https://discord.com/api/webhooks/1141432404167364721/Jy3cIsHbSwq7HaqMxD0E7i_pWrtiy_kBlFyNtcyzi-2nnnitkFUujzO7mTwzTpuq3Lto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"content":"Kutty kutty time!"}),
+    });
     button.style.display = "none";
     marquee.textContent = "And the Baby shall appear...";
   });
